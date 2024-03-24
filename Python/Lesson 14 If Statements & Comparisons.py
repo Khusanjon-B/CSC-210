e3046f282617f2227405124a8ce518a408b91b2b
Khusanjon-B <khusanjon.business@gmail.com>
2024-03-24 14:41:19 -0700

    New Python Lessons

--- Python/Lesson 14 If Statements & Comparisons.py
+++ Python/Lesson 14 If Statements & Comparisons.py
@@ -5,7 +5,7 @@
 def max_num(num1,num2,num3):
     if num1 >=num3 and num1 >= num3:
         return num1
-    elif num2 >= num1 and num2 >= num3:
+    elif (num2 >= num1) and num2 >= num3:
         return num2
     else:
         return num3
