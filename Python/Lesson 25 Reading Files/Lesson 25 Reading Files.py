8a9db947a5a21c722d6902b755ee57df1257e034
Khusanjon-B <khusanjon.business@gmail.com>
2024-03-24 15:30:23 -0700

    New lessons

---
+++ Python/Lesson 25 Reading Files/Lesson 25 Reading Files.py
@@ -0,0 +1,22 @@
+# Reading Files
+
+#Reading from external files using python read command
+
+#For the left section type / to get window where you can browse files
+
+#the right section "r" give the command to just read to file
+
+#Another option is "w" which is write, which means you can change the file
+
+#"a" means append, you can add more stuff, but you can't change stuff
+
+#"r+" give you the method of reading and writing
+
+employee_file = open('/Python/Lesson 25 Reading Files/employees.txt',"r")
+
+#How to check if a file is readable - the function below returns a boolean value
+print(employee_file.readable())
+
+
+#whenever you open a file, you also want to close it
+employee_file.close()
